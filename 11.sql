24eec73d0c6019b08359e776467b377daaffea12
Will Krzastek <157729571+wkraz@users.noreply.github.com>
2024-06-27 07:08:04 -0400

    Delete 11.sql

--- 11.sql
+++
@@ -1,1 +0,0 @@
-SELECT movies.title FROM stars JOIN movies ON stars.movie_id = movies.id JOIN ratings ON stars.movie_id = ratings.movie_id JOIN people ON stars.person_id = people.id WHERE people.name = 'Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
